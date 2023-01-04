1f0d8f8076a41920fb6133dc3a2d44e4b9180a1a
annask94 <annasobieraj3@gmail.com>
2023-01-04 16:11:43 +0100

    build: start of the function of the player choice

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+// Computer choice
 let getComputerChoice = () => {
   const variants = ["Paper", "Rock", "Scissors"];
   let computerChoice = variants[Math.floor(Math.random() * variants.length)];
   return computerChoice;
 };
 
-console.log(getComputerChoice());
+// Player choice
+
+let getPlayerChoice = prompt("What is your choice? Paper, Rock or Scissors");
+
+function playerSelection(choice) {
+  let getPlayerChoice = prompt("What is your choice? Paper, Rock or Scissors");
+
+  let choice = getPlayerChoice.toUpperCase;
+  switch (choice) {
+    case "PAPER":
+      return "Paper";
+      break;
+    case "ROCK":
+      return "Rock";
+      break;
+    case "SCISSORS":
+      return "Scissors";
+      break;
+    default:
+      return "Wrong choice, try again";
+      break;
+  }
+}
+
+console.log(playerSelection());
