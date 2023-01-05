0c4bd0968013cdf0327008b2f0044177ba401e4c
annask94 <annasobieraj3@gmail.com>
2023-01-06 00:57:57 +0100

    fix: fix of random choice and playRound
    Final function playRound works.

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 // Computer choice
-let getComputerChoice = () => {
-  const variants = ["Paper", "Rock", "Scissors"];
-  let computerChoice = variants[Math.floor(Math.random() * variants.length)];
-  return computerChoice;
-};
+function getComputerChoice() {
+  const variants = ["PAPER", "ROCK", "SCISSORS"];
+  return variants[Math.floor(Math.random() * variants.length)];
+}
 
 // Player choice
-
 let playerSelection = prompt("What is your choice? Paper, Rock or Scissors");
-
 // GAME
 
-function playRound(playerSelection, getComputerChoice) {
-  let playerChoice = playerSelection.toUpperCase;
-  let computerChoice = getComputerChoice.toUpperCase;
-  if (playerChoice === computerChoice) {
+function playRound(a, b) {
+  let playerChoice = playerSelection.toUpperCase();
+  let computerChoice = getComputerChoice();
+  if (playerChoice == computerChoice) {
     return "Deaw!";
-  } else if (playerSelection === "PAPER" && getComputerChoice === "ROCK") {
+  } else if (playerChoice == "PAPER" && computerChoice == "ROCK") {
     return "You won! PAPER beats ROCK";
-  } else if (playerSelection === "PAPER" && getComputerChoice === "SCISSORS") {
+  } else if (playerChoice == "PAPER" && computerChoice == "SCISSORS") {
     return "You lose! SCISSORS beats PAPER";
-  } else if (playerSelection === "ROCK" && getComputerChoice === "PAPER") {
+  } else if (playerChoice == "ROCK" && computerChoice == "PAPER") {
     return "You lose! PAPER beats ROCK";
-  } else if (playerSelection === "ROCK" && getComputerChoice === "SCISSORS") {
+  } else if (playerChoice == "ROCK" && computerChoice == "SCISSORS") {
     return "You win! ROCK beats SCISSORS";
-  } else if (playerSelection === "SCISSORS" && getComputerChoice === "PAPER") {
+  } else if (playerChoice == "SCISSORS" && computerChoice == "PAPER") {
     return "You win! SCISSORS beats PAPER";
-  } else if (playerSelection === "SCISSORS" && getComputerChoice === "ROCK") {
+  } else if (playerChoice == "SCISSORS" && computerChoice == "ROCK") {
     return "You lose! ROCK beats SCISSORS";
   } else {
     alert("Wrong choice, try again");
